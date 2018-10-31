ac5fa1c06cb40045eeee734012089dfc8078c44b
Sven Grunewaldt <sven.grunewaldt@lab25.de>
2018-10-31 09:05:09 +0100

    Add project generated by mix new

---
+++ lib/plug_anonymize_ip.ex
@@ -0,0 +1,18 @@
+defmodule PlugAnonymizeIp do
+  @moduledoc """
+  Documentation for PlugAnonymizeIp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugAnonymizeIp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_anonymize_ip_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugAnonymizeIpTest do
+  use ExUnit.Case
+  doctest PlugAnonymizeIp
+
+  test "greets the world" do
+    assert PlugAnonymizeIp.hello() == :world
+  end
+end
